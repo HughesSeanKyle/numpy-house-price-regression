0b1369c5a4bc445da83eae9ae318bd094466d22b
HughesSeanKyle <khughessean@yahoo.com>
2026-09-15 09:22:05 +0200

    Step 5: append_column

--- model.py
+++ model.py
@@ -42,8 +42,11 @@ def make_ratio_feature(numerator, denominator, eps=1e-8):
     return ratio
     pass
 
-# Step 5 - append_column (not yet solved)
-# TODO: implement
+# Step 5 - append_column
+def append_column(X, col):
+    # TODO: Horizontally append one 1-D feature column onto a design matrix.
+    return np.column_stack([X, col])
+    pass
 
 # Step 6 - one_hot_encode (not yet solved)
 # TODO: implement
